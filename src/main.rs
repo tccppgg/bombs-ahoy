a26505d53ac9a7291d80250174504be19cff3691
jazzfool <saveuselon@gmail.com>
2020-12-23 03:38:24 +1100

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+
+fn funny(commands: &mut Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(CameraUiBundle::default()).spawn(TextBundle {
+        style: Style {
+            align_self: AlignSelf::Center,
+            ..Default::default()
+        },
+        text: Text {
+            value: String::from("Bombs Ahoy! :)"),
+            font: asset_server.load("fonts/Langar-Regular.ttf"),
+            style: TextStyle {
+                font_size: 60.,
+                color: Color::WHITE,
+                ..Default::default()
+            },
+        },
+        ..Default::default()
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(funny.system())
+        .run();
 }
